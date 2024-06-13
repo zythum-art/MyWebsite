e8e73d628deece9371a3d408529c3ce6f45febd2
zythum-art <yzhan959@asu.edu>
2024-06-13 14:21:55 -0700

    initiate

---
+++ MyWebsite/src/main/java/com/Cason/MyWebsite/MyWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.Cason.MyWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebsiteApplication.class, args);
+	}
+
+}
---
+++ MyWebsite/src/test/java/com/Cason/MyWebsite/MyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cason.MyWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
