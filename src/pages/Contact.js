edda89855050eda2d3ee289705d7bca63d3b0d73
zythum-art <yzhan959@asu.edu>
2024-06-14 13:52:11 -0700

    Add contact page with email and Gmail logo

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Contact.css';
+import gmailIcon from './Gmail_icon.png';
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>This is the contact page.</p>
+    <div className="contact">
+      <h1>Contact Me</h1>
+      <p>You can reach out to me via email:</p>
+      <div className="contact-info">
+        <img src={gmailIcon} alt="Gmail" />
+        <a href="mailto:zyt1726660473@gmail.com">zyt1726660473@gmail.com</a>
+      </div>
     </div>
   );
-};
+}
 
 export default Contact;
